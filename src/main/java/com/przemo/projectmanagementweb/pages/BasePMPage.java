c17a327f092554073a099c752764b3b081046e57
ziaziek <ziaziek@poczta.fm>
2016-07-05 13:20:22 +0200

    CSS attached and implemented. Further work will be required to sort out some style issues still, beyond the scope, though.

--- src/main/java/com/przemo/projectmanagementweb/pages/BasePMPage.java
+++ src/main/java/com/przemo/projectmanagementweb/pages/BasePMPage.java
@@ -20,41 +20,57 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
  * @author Przemo
  */
 public class BasePMPage extends WebPage {
-    
+
     @SpringBean
     private LoginService loginService;
-    
+
     private FeedbackPanel fpanel;
+
+    private final Class[] linkPages = new Class[]{HomePage.class, SprintsListPage.class, TasksPage.class, BacklogPage.class, ProjectsPage.class};
+    private final String[] linkNames = new String[]{"homeLink", "sprintsLink", "tasksLink", "backlogLink", "projectsLink"};
     
     public BasePMPage() {
         super();
         initPage();
     }
-    
+
     public BasePMPage(IModel model) {
         super(model);
         initPage();
-    }   
-    
-    protected Users getCurrentUser(){
+    }
+
+    protected Users getCurrentUser() {
         return loginService.getLoggedInUser();
     }
-    
+
     private void initPage() {
         fpanel = new FeedbackPanel("feedback");
         add(fpanel);
         if (loginService.isLoggedIn()) {
+            addTopMenuLinks();
             add(new LoginStatusPanel("loginStatusPanel", new CompoundPropertyModel<>(loginService.getLoggedInUser())));
             add(new Link("homeButton") {
                 @Override
                 public void onClick() {
                     setResponsePage(HomePage.class);
                 }
-                
+
             });
         } else {
             setResponsePage(LoginPage.class);
         }
-        
+
+    }
+
+    private void addTopMenuLinks() {
+        for(int i=0; i< linkPages.length; i++){
+            final Class p = linkPages[i];
+            add(new Link(linkNames[i]) {          
+                @Override
+                public void onClick() {
+                    setResponsePage(p);
+                }
+            });
+        }
     }
 }
