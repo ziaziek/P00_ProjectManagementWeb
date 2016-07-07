a233c6a4fc116418e2159d1839dabb1502b3b9e0
ziaziek <ziaziek@poczta.fm>
2016-07-07 21:41:42 +0200

    WIP

--- src/main/java/com/przemo/projectmanagementweb/pages/BasePMPage.java
+++ src/main/java/com/przemo/projectmanagementweb/pages/BasePMPage.java
@@ -8,53 +8,119 @@ package com.przemo.projectmanagementweb.pages;
 import com.przemo.projectmanagementweb.controls.LoginStatusPanel;
 import com.przemo.projectmanagementweb.domain.Users;
 import com.przemo.projectmanagementweb.services.LoginService;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 /**
  *
  * @author Przemo
  */
-public class BasePMPage extends WebPage {
-    
+public abstract class BasePMPage extends WebPage {
+
     @SpringBean
     private LoginService loginService;
-    
+
     private FeedbackPanel fpanel;
-    
+
+    protected Class[] topMenuClasses = new Class[]{HomePage.class, SprintsListPage.class, TasksPage.class, BacklogPage.class, ProjectsPage.class};
+    protected String[] topMenuNames = new String[]{"Home", "Sprints", "Tasks", "Backlog", "Projects"};
+
     public BasePMPage() {
         super();
         initPage();
     }
-    
+
     public BasePMPage(IModel model) {
         super(model);
         initPage();
-    }   
-    
-    protected Users getCurrentUser(){
+    }
+
+    protected Users getCurrentUser() {
         return loginService.getLoggedInUser();
     }
-    
+
     private void initPage() {
         fpanel = new FeedbackPanel("feedback");
         add(fpanel);
+        addTopMenu();
         if (loginService.isLoggedIn()) {
             add(new LoginStatusPanel("loginStatusPanel", new CompoundPropertyModel<>(loginService.getLoggedInUser())));
-            add(new Link("homeButton") {
-                @Override
-                public void onClick() {
-                    setResponsePage(HomePage.class);
-                }
-                
-            });
         } else {
             setResponsePage(LoginPage.class);
         }
-        
     }
+
+    private void addTopMenu() {
+        add(new ListView<MenuItem>("topMenu", getMenuModel()) {
+
+            @Override
+            protected void populateItem(ListItem<MenuItem> li) {
+                if (getClass().equals(li.getModel().getObject().menuClass)) {
+                    li.add(new AttributeModifier("class", Model.of("current_page_item")));
+                }
+                li.add(new Link("link") {
+                    @Override
+                    public void onClick() {
+                        setResponsePage(li.getModel().getObject().menuClass);
+                    }
+                });
+                li.add(new Label("linkText", Model.of(li.getModel().getObject().menuWicketId)));
+            }
+        }
+        );
+    }
+
+    private IModel getMenuModel() {
+        List<MenuItem> menuList = new ArrayList();
+        for (int i = 0; i < topMenuClasses.length; i++) {
+            MenuItem m = new MenuItem();
+            m.menuClass = topMenuClasses[i];
+            m.menuWicketId = topMenuNames[i];
+            menuList.add(m);
+        }
+        MenuModel model = new MenuModel();
+        model.setObject(menuList);
+        return model;
+    }
+
+    class MenuItem {
+
+        Class menuClass;
+        String menuWicketId;
+    }
+
+    class MenuModel implements IModel {
+
+        List<MenuItem> items;
+
+        @Override
+        public Object getObject() {
+            return items;
+        }
+
+        @Override
+        public void setObject(Object t) {
+            if (t instanceof List) {
+                items = (List<MenuItem>) t;
+            }
+        }
+
+        @Override
+        public void detach() {
+            items = null;
+        }
+
+    }
+
 }
