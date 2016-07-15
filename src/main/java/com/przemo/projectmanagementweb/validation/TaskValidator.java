06bf71f5682c318d0850447929efd7bbb9f60c94
ziaziek <ziaziek@poczta.fm>
2016-07-15 15:21:05 +0200

    Status i walidacja zaimplementowane.

---
+++ src/main/java/com/przemo/projectmanagementweb/validation/TaskValidator.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.przemo.projectmanagementweb.validation;
+
+import com.przemo.projectmanagementweb.domain.Task;
+
+/**
+ *
+ * @author Przemo
+ */
+public class TaskValidator {
+    
+    private final Task task;
+    
+    public TaskValidator(Task task){
+        this.task=task;
+    }
+    
+    public boolean validate(){
+        return task!=null && !(task.getSprint()!=null &&
+                task.getStatus().getName().equals("Created") || task.getStatus().getName().equals("Closed"));
+    }
+}
